644c4d375c1b5c78e1bb46a6450f01b7746442b3
Bagelis Bliziotis <el25062@mail.ntua.gr>
2026-05-07 18:28:35 +0300

    Add jump function and implement in main loop

--- 06-reeborg-world/hurdle-2.py
+++ 06-reeborg-world/hurdle-2.py
@@ -1,1 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
 
+while not at_goal():
+    move()
+    jump()
