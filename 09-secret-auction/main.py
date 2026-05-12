3c21b616477eecfe7c0becf621d659fcbf1f77ed
Bagelis Bliziotis <el25062@mail.ntua.gr>
2026-05-12 10:17:52 +0300

    Implement secret auction bidding system

--- 09-secret-auction/main.py
+++ 09-secret-auction/main.py
@@ -1,1 +1,23 @@
+import art
+
+print(art.logo)
+check = True
+players_bids = {}
+
+while check:
+    name = input("What is your name?: ")
+    bid  = int(input("What is your bid?: $"))
+    question = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+    if question == "no":
+        check = False
+    players_bids[name] = bid
+    print("\n"*100)
+    winner = name
+
+for key in players_bids:
+    if players_bids[winner] < players_bids[key]:
+        winner = key
+winning_bid = players_bids[winner]
+
+print(f"The winner is {winner} with a bid of ${winning_bid}")
 
