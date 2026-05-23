c2f9945bf7c1db979c5ad007afc7391c7f4b76d8
Bagelis Bliziotis <el25062@mail.ntua.gr>
2026-05-23 16:27:57 +0300

    Update main.py

--- 18-etch-a-sketch/main.py
+++ 18-etch-a-sketch/main.py
@@ -1,1 +1,34 @@
+from turtle import Turtle
+from turtle import Screen
 
+tim = Turtle()
+screen = Screen()
+
+screen.listen()
+
+def move_f():
+    tim.forward(20)
+
+def move_back():
+    tim.backward(20)
+
+def left():
+    tim.left(10)
+
+def right():
+    tim.right(10)
+
+def clear():
+    screen.reset()
+
+
+
+screen.onkey(clear,"c")
+screen.onkey(left,"a")
+screen.onkey(move_f,"w")
+screen.onkey(right,"d")
+screen.onkey(move_back,"s")
+
+
+
+screen.exitonclick()
