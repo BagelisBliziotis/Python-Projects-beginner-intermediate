7c0fd9fca99af7bc745cb9dfc529328c3874129c
Bagelis Bliziotis <el25062@mail.ntua.gr>
2026-05-25 18:59:29 +0300

    Update scoreboard.py

--- 20-Snake/scoreboard.py
+++ 20-Snake/scoreboard.py
@@ -1,1 +1,25 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier",24,"normal")
+
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.pencolor("white")
+        self.penup()
+        self.goto(-5, 260)
+        self.write(f"Score:{self.score}",align = ALIGNMENT, font = FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over!!" , align =ALIGNMENT, font = FONT)
+
+    def scoring(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score:{self.score}", align=ALIGNMENT , font= FONT)
 
