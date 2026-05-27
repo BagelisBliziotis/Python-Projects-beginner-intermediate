eb71fc0491a6269c2137ab593f8eaf57df3eeac0
Bagelis Bliziotis <el25062@mail.ntua.gr>
2026-05-27 11:32:26 +0300

    Update ball.py

--- 21-Pong/ball.py
+++ 21-Pong/ball.py
@@ -1,1 +1,25 @@
+from turtle import Turtle
 
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.direction1 = 1
+        self.direction2 = 1
+        self.color("white")
+        self.move_speed = 0.1
+    def reset(self):
+        self.goto(0,0)
+        self.bounce2()
+        self.move_speed = 0.1
+
+    def move(self):
+        self.goto(self.xcor() + 10*self.direction2, self.ycor() +10*self.direction1)
+
+    def bounce1(self):
+        self.direction1 *= -1
+
+    def bounce2(self):
+        self.direction2 *= -1
+        self.move_speed *= 0.9
